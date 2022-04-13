be0c41f2993d1b813226d1e479f802e11fbc100a
AdamFASHOO <AdamChapman1994@gmail.com>
2022-04-13 16:50:24 -0400

    Powers Table

---
+++ Powers Table/Program.cs
@@ -0,0 +1,71 @@
+﻿class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Learn your squares and cubes!");
+        bool goAgain = true;
+
+        while (goAgain == true)
+        {
+
+            Console.WriteLine("\nPlease enter an integer:");
+            
+            int num = int.Parse(Console.ReadLine());
+
+            if (num >= 1 && num <= 1290)
+            {
+                Console.WriteLine("\n\tNumber  Squared  Cubed");
+                Console.WriteLine("\t======  =======  ======");
+
+                for (int numStart = 1; numStart <= num; numStart++)
+                {
+                    double square = numStart * numStart;
+                    double cube = numStart * numStart * numStart;
+
+                    Console.WriteLine("\t " + numStart + "\t " + square + "\t  " + cube);
+                }
+            }
+            else if (num > 1290)
+            {
+                Console.WriteLine("That number was too large!\n");
+                continue;
+            }
+            else
+            {
+                Console.WriteLine("That number was too small!\n");
+                continue;
+            }
+
+            goAgain = RunAgain();
+        }
+    }
+
+    public static string GetUserInput(string prompt)
+    {
+        Console.WriteLine(prompt);
+        string input = Console.ReadLine();
+        return input;
+
+    }
+    public static bool RunAgain()
+    {
+        string answer = GetUserInput("\n\nWould you like to go again? y/n ").ToLower();
+
+        if (answer == "y")
+        {
+            return true;
+        }
+        else if (answer == "n")
+        {
+            Console.WriteLine("\nGoodbye!");
+            return false;
+        }
+        else
+        {
+            Console.WriteLine("\nI'm sorry, I didn't understand that. \nPlease input y or n next time.");
+            return RunAgain();
+        }
+
+    }
+
+}
